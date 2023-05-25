b32185c5e6b0370d9d081da9fa8e74aba8254f93
ODADJE <odadjerk@gmail.com>
2023-05-25 23:53:49 +0000

    gestion d'erreurs sur la saisi du nombre mystere

--- Codes/Nombre_mystere/script.py
+++ Codes/Nombre_mystere/script.py
@@ -3,18 +3,24 @@ import random
 volonte = "o"
 start = 0
 end = 4
+
 while volonte == "o":
+
+    nombre_de_chance = 3
     nombre_mystere = random.randint(start,end)
-    i = 1
-    while i <= 3:
-        nombre_saisi = int(input(f"Veuillez saisir votre nombre mystere entre {start} et {end} : "))
+    
+    while  nombre_de_chance > 0:
+        print(f"Il vous reste {nombre_de_chance} chance")
+        nombre_saisi = input(f"Veuillez saisir votre nombre mystere entre {start} et {end} : ")
+        if not nombre_saisi.isdigit():
+            continue
+        nombre_saisi = int(nombre_saisi)
         if nombre_saisi == nombre_mystere:
             print("Bien joue!!!")
-            print(f"Vous avez reussi en {i} coup(s)")
             break
-        elif i == 3:
-            print("Essaie une prochane fois!")
-        i+=1
+        nombre_de_chance-=1
 
+    if nombre_de_chance == 0:
+        print("Essaie une prochane fois!")
     volonte = input("Voulez-vous continer? o/n : ")
 
